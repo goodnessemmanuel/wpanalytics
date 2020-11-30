345b3634c1e32041bc21bfb5edf7895f5dea8173
Oche Ejeh <35895581+goodnessemmanuel@users.noreply.github.com>
2020-11-30 18:29:14 +0100

    add date to index

---
+++ homepage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <div>Wecome to our home page</div>
+    <form action="">
+        <input type="text" name="" id="">
+        <input type="text" name="" id="">
+        <input type="text" name="" id="">
+    </form>
+</body>
+</html>
--- public/index.php
+++ public/index.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "Welcome to our site";
+echo "Welcome to our Site " . date("Y-m-d H:i:s");
